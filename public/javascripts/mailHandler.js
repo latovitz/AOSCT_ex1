b548abb729a10e74fb71077835510e321c682b3a
latovitz <orlatovitz@gmail.com>
2016-03-24 12:47:17 +0200

    upload as main 3

---
+++ public/javascripts/mailHandler.js
@@ -0,0 +1,35 @@
+﻿//handle mail send
+
+var nodemailer = require('nodemailer');
+
+exports.sendMail = function (recipient, file){
+    // create reusable transporter object using the default SMTP transport
+    var transporter = nodemailer.createTransport('smtps://aosct1%40gmail.com:AOSCTAOSCT@smtp.gmail.com');
+    
+    var reader = new FileReader();
+    reader.onload = function (e){
+        var fileContent = e.target.result;
+    }
+
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: '"Or Lato" <aosct1@gmail..com>', // sender address
+        to: recipient, // list of receivers
+        subject: 'Hello, File 4 U ✔', // Subject line
+        text: 'Hey There 🐴', // plaintext body
+        attachments: [
+            {
+                filename: file.name,
+
+            }
+        ]
+    };
+    
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+}
